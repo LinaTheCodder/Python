619587f29ea8ea1b830584a29487c0c58123a049
LinaTheCodder <lina.dhanani2010@gmail.com>
2025-08-27 10:39:30 +0530

    calculator.py
    Perform Basic Mathematical Operations

---
+++ calculator.py
@@ -0,0 +1,7 @@
+value1=int(input('Enter the first number '))
+value2=int(input('Enter the second number '))
+print("Addition:" , int(value1+value2))
+print("Substraction:" , int(value1-value2))
+print("Multiplication:" , int(value1*value2))
+print("Division:" , int(value1/value2))
+
